ec47cd12767734366be71e6ed7475254b65967a0
KevinGibson1 <82619429+KevinGibson1@users.noreply.github.com>
2021-04-27 17:10:47 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,103 @@
 #include <iostream>
+#include "matrixswap.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    matrix m[3][3], b[3][3];
+    matrix s1, s2, s3, s4, test;
+    matrix s5, s6, s7, s8, s9, z[3][3];
+    int x;
+    bool addsUp = false;
+
+
+
+    int small1[3][3] = {{5,3,4}, {6,7,2},{1,9,8}};
+    int small2[3][3] = {{6,7,8}, {1,9,5},{3,4,2}};
+    int small3[3][3] = {{9,1,2}, {3,4,8},{5,6,7}};
+    int small4[3][3] = {{8,5,9}, {4,2,6},{7,1,3}};
+    int small5[3][3] = {{7,6,1}, {8,5,3},{9,2,4}};
+    int small6[3][3] = {{4,2,3}, {7,9,1},{8,5,6}};
+    int small7[3][3] = {{9,6,1}, {2,8,7},{3,4,5}};
+    int small8[3][3] = {{5,3,7}, {4,1,9},{2,8,6}};
+    int small9[3][3] = {{2,8,4}, {6,3,5},{1,7,9}};
+
+
+    s1.readS();
+
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            cout << small1[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    s1.fillSmallMatrix(small1);
+    s2.fillSmallMatrix(small2);
+    s3.fillSmallMatrix(small3);
+    s4.fillSmallMatrix(small4);
+    s5.fillSmallMatrix(small5);
+    s6.fillSmallMatrix(small6);
+    s7.fillSmallMatrix(small7);
+    s8.fillSmallMatrix(small8);
+    s9.fillSmallMatrix(small9);
+
+    m[0][0] = s1;
+    m[0][1] = s2;
+    m[0][2] = s3;
+    m[1][0] = s4;
+    m[1][1] = s5;
+    m[1][2] = s6;
+    m[2][0] = s7;
+    m[2][1] = s8;
+    m[2][2] = s9;
+
+    check(m);
+
+    x = s1.getS(0,0);
+    cout << x << endl;
+    cout << "READ TOP LEFT" << endl;
+    s2.readS();
+    x = s2.getS(0,0);
+    cout << x << endl;
+
+    b[0][0] = m[0][0];
+    test = b[0][0];
+    cout << "read test" << endl;
+    test.readS();
+    z[3][3] = Random(m);
+
+    z[0][0] = m[0][0];
+    test = z[0][0];
+    cout << "read test z" << endl;
+    test.readS();
+
+
+    cout << endl << endl;
+
+    x = m[0][0].getS(2,2);
+    cout << x << endl;
+
+
+    // THE TEST DISPLAY FUNCTION
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            m[i][j].readS();
+            cout << endl;
+        }
+
+    }
+
+    addsUp = check(m);
+
+
+    cout << "does it add up? " << addsUp;
+
+    cout << endl << endl;
+
+    //m[3][3].display();
+
+
     return 0;
 }
--- matrixswap.cpp
+++ matrixswap.cpp
@@ -5,14 +5,83 @@
 #include "matrixswap.h"
 
 
-void Randomization (int a[][3]){
+/*
+IF THIS DOESNT WORK
+transform all of the s matracies by reassigning them to t1 - t9
+then at the end put them back to the s's
+*/
+
+//I think i will need to make a matrix object, then put one small matrix in each object;
+//create a function to fill 9 of these matracies
+//then put the matrix object in each position of the large matrix
+// i will also need to change the matrix swap to matrix objects, not integer matracies
+
+
+
+
+
+matrix::matrix(){
+    s[3][3];
+}
+
+void matrix::fillSmallMatrix(int a[3][3]){
+
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+                s[i][j] = a[i][j];
+        }
+    }
+}
+
+
+int matrix::getS(int row, int col){
+    int x;
+    x = s[row][col];
+    return x;
+}
+
+void matrix::readS(){
+
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            cout << s[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+/*
+void display(){
+
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            m[i][j].readS();
+        }
+
+    }
+}
+*/
+
+matrix Random(matrix a[3][3]){
+
+    matrix b[3][3];
+    matrix d[3][3];
+
+    srand(time(0));
+
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            b[i][j] = a[i][j];
+            d[i][j] = a[i][j];
+        }
+    }
+
 
     int iterations = rand() % 10;
     cout << "iterations" << iterations << endl;
     int f;
 
     for (f = 0; f <= iterations; f++){
-        int b[3][3];
+
         int c[3];
         c[0] = rand() % 3;
 
@@ -31,15 +100,27 @@ void Randomization (int a[][3]){
         for (int i = 0; i < 3; i++){
             for( int j = 0; j < 3; j++){
                 int temp = c[i];
+
+
+
+                // create a get function to fill b[i][j]
+
+
+
                 b[i][j] = a[temp][j];
-                cout << " " << b[i][j] << " ";
+                //cout << " " << b[i][j] << " ";
             }
-            cout << endl;
+            //cout << endl;
         }
 
-        a = b;
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                a[i][j] = b[i][j];
 
-        int d[3][3];
+            }
+        }
+
+
         int r[3];
         r[0] = rand() % 3;
 
@@ -59,12 +140,73 @@ void Randomization (int a[][3]){
             for( int j = 0; j < 3; j++){
                 int temp = r[j];
                 d[i][j] = a[i][temp];
-                cout << " " << d[i][j] << " ";
+               //cout << " " << d[i][j] << " ";
             }
-            cout << endl;
+            //cout << endl;
         }
 
-        a = d;
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                a[i][j] = d[i][j];
+
+            }
+        }
+
     }
 
+    return a[3][3];
 }
+
+
+
+
+
+bool check(matrix a[3][3]){
+    bool works = true;
+    string work = "true";
+    int r1, r2, r3, c1 , c2, c3;
+
+    for (int k = 0; k < 3; k++){
+        r1 = 0, r2 = 0, r3 = 0;
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                r1 += a[k][i].getS(0,j);
+                r2 += a[k][i].getS(1,j);
+                r3 += a[k][i].getS(2,j);
+            }
+        }
+        //cout << r1 << " - " << r2 << " - " << r3 << endl;
+        if ((r1 != 45) || (r2 != 45) || (r3 != 45)){
+            works = false;
+            //work = "false";
+
+        }
+    }
+    //cout <<" row" << work << endl;
+
+    for (int k = 0; k < 3; k++){
+        c1 = 0, c2 = 0, c3 = 0;
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                c1 += a[i][k].getS(j,0);
+                c2 += a[i][k].getS(j,1);
+                c3 += a[i][k].getS(j,2);
+                //a[i][k].readS();
+                //cout << c1 << " - " << c2 << " - " << c3 << endl;
+            }
+
+        }
+
+        //cout << c1 << " - " << c2 << " - " << c3 << endl;
+        if ((c1 != 45) || (c2 != 45) || (c3 != 45)){
+            works = false;
+            //work = "false";
+
+        }
+    }
+    //cout << "col" << work << endl;
+    return works;
+}
+
+
+
